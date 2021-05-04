2c8e8571fb2d67a37f62e5c33bd310fe1d1613d1
Roma <sikarev.roman@yandex.ru>
2021-05-04 12:09:02 +0300

    copied files from lesson 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = () => {
+    return <header className='header'>
+        <img src='https://api.freelogodesign.org/assets/thumb/logo/1492486_400.png' placeholder='image' />
+    </header>
+}
+
+export default Profile;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://static-cse.canva.com/blob/185563/56.-Sunkissed-Village.f8ebc04e.png' />
+        </div>
+        <div>
+            avatar + description
+    </div>
+        <div>
+            My posts
+      <div>
+                New posts
+      </div>
+            <div>
+                <div>
+                    post 1
+      </div>
+                <div>
+                    post 2
+      </div>
+                <div>
+                    post 3
+      </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
