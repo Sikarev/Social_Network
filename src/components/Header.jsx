c3e19b4fc0faa2d4a688960fc57ebb4585ac4883
Roma <sikarev.roman@yandex.ru>
2021-05-04 14:24:56 +0300

    L14 css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Profile = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
         <img src='https://api.freelogodesign.org/assets/thumb/logo/1492486_400.png' placeholder='image' />
     </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={classes.content}>
         <div>
             <img src='https://static-cse.canva.com/blob/185563/56.-Sunkissed-Village.f8ebc04e.png' />
         </div>
         <div>
             avatar + description
-    </div>
+        </div>
         <div>
             My posts
-      <div>
-                New posts
-      </div>
             <div>
-                <div>
+                New posts
+            </div>
+            <div className={classes.posts}>
+                <div className={classes.item}>
                     post 1
-      </div>
-                <div>
+                </div>
+                <div className={classes.item}>
                     post 2
-      </div>
-                <div>
+                </div>
+                <div className={classes.item}>
                     post 3
-      </div>
+                </div>
             </div>
         </div>
     </div>
