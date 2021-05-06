fdb446d8db87273955a4518be253e3e14505cf03
Roma <sikarev.roman@yandex.ru>
2021-05-06 17:35:04 +0300

    installed react-router-dom module, added routing + a few padding for better look

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import './zero.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts'
 import classes from './Profile.module.css';
 
 const Profile = () => {
-    return <div className={classes.content}>
+    return <div>
         <div>
             <img src='https://static-cse.canva.com/blob/185563/56.-Sunkissed-Village.f8ebc04e.png' />
         </div>
