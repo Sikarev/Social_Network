b4f69654df45a1fcae3b82f4cc2faf5693b8d705
Roma <sikarev.roman@yandex.ru>
2021-05-07 16:58:23 +0300

    L21 created dialogs layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import classes from './Dialogs.module.css';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Katya
+                </div>
+                <div className={s.dialog}>
+                    Mark
+                </div>
+                <div className={s.dialog}>
+                    John
+                </div>
+                <div className={s.dialog}>
+                    Sara
+                </div>
+                <div className={s.dialog}>
+                    Michael
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How your doing?</div>
+                <div className={s.message}>Send nudes pls</div>
+            </div>
         </div>
     )
 }
